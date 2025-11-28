d85aba64bf7c8a6ac189a80030d36fed50e11bb9
charan-naga-deep-puppala <133045820+charan-naga-deep-puppala@users.noreply.github.com>
2025-11-28 08:53:33 +0530

    Refactor server.js for improved configuration

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { GoogleGenAI } = require("@google/genai");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -129,4 +129,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`GenAI Server running on http://localhost:${port}`);
+
 });
